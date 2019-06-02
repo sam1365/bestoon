7b40ec128656fe8ba4451e6f3c01294c91025080
sam <moradi.sayyedali@gmail.com>
2019-06-02 15:16:04 +0430

    Incom has created

--- web/models.py
+++ web/models.py
@@ -7,3 +7,9 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+class Income(models.Model):
+    text = models.CharField(max_length = 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete = models.PROTECT)
